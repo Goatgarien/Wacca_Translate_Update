25cb87e98fbf0bebd5e7707f7bb2507828fe8631
Michael Cluff <mcluffington@gmail.com>
2022-02-26 21:20:58 -0800

    Add project files.

---
+++ Wacca_Translate_Update/main.cpp
@@ -0,0 +1,24 @@
+#include "json.hpp"
+#include <iostream>
+#include <fstream>
+
+// for convenience
+using json = nlohmann::json;
+using std::ifstream;
+using std::ofstream;
+
+
+void main() {
+	// read a JSON file
+	ifstream inputFile("input.json");
+	json inputJSON;
+	inputFile >> inputJSON;
+
+	ifstream inputFileNew("output.json");
+	json outputJSON;
+	inputFileNew >> outputJSON;
+
+
+	std::ofstream o("pretty.json");
+	o << std::setw(4) << inputJSON << std::endl;
+}
